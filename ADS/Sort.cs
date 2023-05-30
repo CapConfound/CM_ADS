1bd3b9a850e3c40804d158647a9bd5b9ad113a82
Ilya Pavlov <ilya18052000@gmail.com>
2023-05-30 23:48:58 +0300

    fixed sort

--- ADS/Sort.cs
+++ ADS/Sort.cs
@@ -1,6 +1,6 @@
 class Sort {
 
-    private static T FindMaxEl<T> (T[] list, int limit) where T : IComparable
+    private static T FindMaxEl<T> (T[] list) where T : IComparable
     {
         T max = list[0];
         foreach(T el in list) {
@@ -106,24 +106,47 @@ class Sort {
         }
     }
 
-    public static void RadixSort (int[] arr)
+    private static void CountSort(int[] arr, int size, int exp)
     {
-        int i, j;
-        int[] tmp = new int[arr.Length];
-        for (int shift = 31; shift > -1; --shift)
+        int[] result = new int[size];
+        int[] count = new int[10];
+
+        for (int i = 0; i < 10; i++)
         {
-            j = 0;
-            for (i = 0; i < arr.Length; ++i)
-            {
-                bool move = (arr[i] << shift) >= 0;
-                if (shift == 0 ? !move : move)   
-                    arr[i-j] = arr[i];
-                else                             
-                    tmp[j++] = arr[i];
-            }
-            Array.Copy(tmp, 0, arr, arr.Length-j, j);
+            count[i] = 0;
         }
 
+        for (int i = 0; i < size; i++)
+        {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        // Сдвиг обратно для выравнивания
+        for (int i = 1; i < 10; i++)
+        {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = size - 1; i >= 0; i--)
+        {
+            result[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = result[i];
+        }
+    }
+    
+    public static void RadixSort (int[] arr, int size)
+    {
+        int max = FindMaxEl(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10)
+        {
+            CountSort(arr, size, exp);
+        }
     }
 
     // TODO: quick sort
@@ -157,10 +180,9 @@ class Sort {
             Quicksort(array, leftIndex, j);
         if (i < rightIndex)
             Quicksort(array, i, rightIndex);
-        return;
     }
 
-    // TODO: recursive merge sort 
+    // Recursive merge sort 
     private static void MergeSortInner<T> (T[] array, int left, int middle, int right) where T : IComparable
     {
         int leftArrayLength = middle - left + 1;
@@ -168,26 +190,36 @@ class Sort {
         T[] leftTempArray = new T[leftArrayLength];
         T[] rightTempArray = new T[rightArrayLength];
         int i, j;
+        int k = left;
+        
         for (i = 0; i < leftArrayLength; ++i)
+        {
             leftTempArray[i] = array[left + i];
+        }
         for (j = 0; j < rightArrayLength; ++j)
+        {
             rightTempArray[j] = array[middle + 1 + j];
+        }
         i = 0;
         j = 0;
-        int k = left;
+        
         while (i < leftArrayLength && j < rightArrayLength)
         {
             if (rightTempArray[j].CompareTo(leftTempArray[i]) >= 0) // (leftTempArray[i] <= rightTempArray[j])
             {
                 array[k++] = leftTempArray[i++];
-            } else {
+            } 
+            else 
+            {
                 array[k++] = rightTempArray[j++];
             }
         }
+        
         while (i < leftArrayLength)
         {
             array[k++] = leftTempArray[i++];
         }
+        
         while (j < rightArrayLength)
         {
             array[k++] = rightTempArray[j++];
@@ -206,20 +238,12 @@ class Sort {
         return;
     }
 
-    //recursiveless merge sort
+    // Нерекурсивная сортировка слиянием (merge sort)
     public static void MergeSort<T> (T []arr, int n) where T : IComparable
     {
-          
-        // For current size of subarrays to
-        // be merged curr_size varies from
-        // 1 to n/2
         int curr_size;
-                      
-        // For picking starting index of
-        // left subarray to be merged
         int left_start;
-                          
-          
+
         // Merge subarrays in bottom up
         // manner. First merge subarrays
         // of size 1 to create sorted
@@ -227,87 +251,24 @@ class Sort {
         // subarrays of size 2 to create
         // sorted subarrays of size 4, and
         // so on.
-        for (curr_size = 1; curr_size <= n-1;
-                      curr_size = 2*curr_size)
+        // 
+        for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size)
         {
-              
             // Pick starting point of different
             // subarrays of current size
-            for (left_start = 0; left_start < n-1;
-                        left_start += 2*curr_size)
+            for (left_start = 0; left_start < n - 1;
+                        left_start += 2 * curr_size)
             {
                 // Find ending point of left
                 // subarray. mid+1 is starting
                 // point of right
-                int mid = Math.Min(left_start + curr_size - 1,n-1);
-          
-                int right_end = Math.Min(left_start
-                             + 2*curr_size - 1, n-1);
-          
+                int mid = Math.Min(left_start + curr_size - 1, n - 1);
+                int right_end = Math.Min(left_start + 2 * curr_size - 1, n - 1);
+
                 // Merge Subarrays arr[left_start...mid]
                 // & arr[mid+1...right_end]
-                Merge(arr, left_start, mid, right_end);
+                MergeSortInner(arr, left_start, mid, right_end);
             }
         }
     }
-      
-    /* Function to merge the two haves arr[l..m] and
-    arr[m+1..r] of array arr[] */
-    public static void Merge<T> (T []arr, int l, int m, int r) where T : IComparable
-    {
-        int i, j, k;
-        int n1 = m - l + 1;
-        int n2 = r - m;
-      
-        /* create temp arrays */
-        T []L = new T[n1];
-        T []R = new T[n2];
-      
-        /* Copy data to temp arrays L[]
-        and R[] */
-        for (i = 0; i < n1; i++)
-            L[i] = arr[l + i];
-        for (j = 0; j < n2; j++)
-            R[j] = arr[m + 1+ j];
-      
-        /* Merge the temp arrays back into
-        arr[l..r]*/
-        i = 0;
-        j = 0;
-        k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i].CompareTo(R[j]) <= 0) //(L[i] <= R[j])
-            {
-                arr[k] = L[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-      
-        /* Copy the remaining elements of
-        L[], if there are any */
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-      
-        /* Copy the remaining elements of
-        R[], if there are any */
-        while (j < n2)
-        {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-    
-    
 }
